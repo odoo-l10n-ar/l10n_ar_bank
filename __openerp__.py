c9d849f23cc25d940233b066fdbc250c8fb7220c
Bruno M. Villasanti <bvillasanti@thymbra.com>
2011-09-14 19:51:17 -0300

    [ADD][6.0.3] Financial Entities from Argentina - l10n_ar_bank module [TODO] Updater script from http://www.bcra.gov.ar/sisfin/sf010100.asp
    Thanks to Cristian S. Rocha

---
+++ __openerp__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2008-2011  Luis Falcon
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Banks of Argentina',
+    'version': '1.0',
+    'author': 'Thymbra - Torre de Hanoi',
+    'category': 'Localisation/Argentina',
+    'website': 'http://www.thymbra.com/',
+    'license': 'GPL-3',
+    'description': """
+Localization Model for Argentina
+Includes:
+ - Financial Entities (http://www.bcra.gov.ar/)
+ 
+""",
+    'depends': [
+        'base',
+        'l10n_ar_states',
+    ],
+    'init_xml': [],
+    'demo_xml': [],
+    'update_xml': [
+        'data/res_bank.xml',
+    ],
+    'active': False,
+    'installable': True,
+}
